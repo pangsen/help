860de40f0931fa53f2482a70e69d4377bc88554a
pangsen <pangsen0014@163.com>
2021-11-27 11:32:16 +0800

    test run js script

--- workflow_scripts/test.js
+++ workflow_scripts/test.js
@@ -1,6 +1,23 @@
 
-async function test(){
+
+var fs = require('fs');
+var path = require('path');
+async function test() {
     console.log("hello world!");
 }
+async function testEditfile() {
+    var filepath = path.normalize(path.join(__dirname, "../package.json"));
+    fs.readFile(filepath, "utf8", async (err, data) => {
+        data = data.split("\r\n");
+        console.log(data.length);
+        data.push("//sam test");
+        await fs.writeFileSync(filepath, data.join("\r\n"));
+        fs.readFile(filepath, "utf8", async (err, data) => {
+            console.log(data);
+        });
+    });
 
-test();
+
+}
+
+testEditfile();
