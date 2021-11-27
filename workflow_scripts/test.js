d849d94b0bf90459080288e3ba6613017fe5c4fd
pangsen <pangsen0014@163.com>
2021-11-27 12:09:16 +0800

    test run js script

--- workflow_scripts/test.js
+++ workflow_scripts/test.js
@@ -1,23 +1,26 @@
 
+console.log(process.argv)
+// var fs = require('fs');
+// var path = require('path');
+// async function test() {
+//     console.log("hello world!");
+// }
+// async function testEditfile() {
+//     var filepath = path.normalize(path.join(__dirname, "../package.json"));
+//     fs.readFile(filepath, "utf8", async (err, data) => {
+//         data = data.split("\r\n");
+//         console.log(data.length);
+//         data.push("//sam test");
+//         await fs.writeFileSync(filepath, data.join("\r\n"));
+//         fs.readFile(filepath, "utf8", async (err, data) => {
+//             console.log(data);
+//         });
+//     });
 
-var fs = require('fs');
-var path = require('path');
-async function test() {
-    console.log("hello world!");
-}
-async function testEditfile() {
-    var filepath = path.normalize(path.join(__dirname, "../package.json"));
-    fs.readFile(filepath, "utf8", async (err, data) => {
-        data = data.split("\r\n");
-        console.log(data.length);
-        data.push("//sam test");
-        await fs.writeFileSync(filepath, data.join("\r\n"));
-        fs.readFile(filepath, "utf8", async (err, data) => {
-            console.log(data);
-        });
-    });
 
+// }
+// async function fileNotAllowToModify(){
 
-}
+// }
 
-testEditfile();
+// testEditfile();
