a1bbc7631b0861cdb23a084fe019548b43398fa3
pangsen <pangsen0014@163.com>
2021-11-28 18:15:04 +0800

    Update node.js.yml

--- workflow_scripts/notAllowDeleteHalfWidthSpace.js
+++ workflow_scripts/notAllowDeleteHalfWidthSpace.js
@@ -4,12 +4,16 @@ var fs = require("fs");
 const rootFolder = process.argv[1].endsWith("workflow_scripts/notAllowDeleteHalfWidthSpace.js") ? "../" : "../../";
 for (let index = 2; index < process.argv.length; index++) {
     var filePath = path.normalize(path.join(__dirname, rootFolder + process.argv[index]))
+    
+    console.log(filePath);
     if (path.extname(filePath) == ".md") {
         var fileInPRBranch = fs.readFileSync(filePath, "utf8");
         var fileInMainBranch = fs.readFileSync(filePath.replace("\\pr\\","\\main\\"), "utf8");
         pr = fileInPRBranch.split("\r\n");
         main = fileInMainBranch.split("\r\n");
         main.forEach((value, index) => {
+            console.log(value);
+            console.log(pr[index]);
             if (value.endsWith("  ") && !pr[index].endsWith("  ")) {
                 console.log("Risk in delete half-width space!")
             }
